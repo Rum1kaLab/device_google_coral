f5056493ab6061f8016bd86d0bb2880961d1cbed
prpjzz <groovemusic4399@pm.me>
2026-03-06 16:39:05 +0000

    Revert "testing gapps flag = true"
    This reverts commit 90af7cf747f3b44506089a0419fe3999234769e2.

--- afterlife_coral.mk
+++ afterlife_coral.mk
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 AFTERLIFE_MAINTAINER := Rum1ka
 TARGET_ENABLE_BLUR := true
 SYSTEM_OPTIMIZE_JAVA := true
-AFTERLIFE_GAPPS := true
+AFTERLIFE_GAPPS := basic
 TARGET_BOOT_ANIMATION_RES := 1080
 SYSTEMUI_OPTIMIZE_JAVA := true
 TARGET_SUPPORTS_QUICK_TAP := true
